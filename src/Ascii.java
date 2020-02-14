51279eddf2136867c8610347f157530ed4a1c3a9
Mo Alaoudi <mohamed.alaoudi@sjsu.edu>
2020-02-13 21:23:29 -0800

    commit1

---
+++ src/Ascii.java
@@ -0,0 +1,60 @@
+public class Ascii {
+    public static void main(String[] args) {
+        System.out.println(" /\\_/\\      -----");
+        System.out.println("( ' ' )   / Welcome \\");
+        System.out.println("(  -  ) <   to mastering |");
+        System.out.println(" | | |    \\ Java and its tools! /");
+        System.out.println("(__|__|)    ----- \n\n");
+
+
+        System.out.println("                                                  `T\",.`-,\n" +
+                "                                                     '8, :.\n" +
+                "                                              `\"\"`oooob.\"T,.\n" +
+                "                                            ,-`\".)O;8:doob.'-.\n" +
+                "                                     ,..`'.'' -dP()d8O8Yo8:,..`,\n" +
+                "                                   -o8b-     ,..)doOO8:':o; `Y8.`,             ----- \n" +
+                "                                  ,..bo.,.....)OOO888o' :oO.  \".  `-.       / Welcome \\ \n" +
+                "                                , \"`\"d....88OOOOO8O88o  :O8o;.    ;;,b   <  to mastering | \n" +
+                "                               ,dOOOOO\"\"\"\"\"\"\"\"O88888o:  :O88Oo.;:o888d   \\ Java and its tools! / \n " +
+                "                               \"\"888Ob...,-- :o88O88o:. :o'\"\"\"\"\"\"\"Y8OP        ----- \n" +
+                "                               d8888.....,.. :o8OO888:: ::\n" +
+                "                              \"\" .dOO8bo`'',,;O88O:O8o: ::,\n" +
+                "                                 ,dd8\".  ,-)do8O8o:\"\"\"; :::\n" +
+                "                                 ,db(.  T)8P:8o:::::    :::\n" +
+                "                                 -\"\",`(;O\"KdOo::        :::\n" +
+                "                                   ,K,'\".doo:::'        :o:\n" +
+                "                                    .doo:::\"\"\"::  :.    'o:\n" +
+                "        ,..            .;ooooooo..o:\"\"\"\"\"     ::;. ::;.  'o.\n" +
+                "   ,, \"'    ` ..   .d;o:\"\"\"'                  ::o:;::o::  :;\n" +
+                "   d,         , ..ooo::;                      ::oo:;::o\"'.:o\n" +
+                "  ,d'.       :OOOOO8Oo::\" '.. .               ::o8Ooo:;  ;o:\n" +
+                "  'P\"   ;  ;.OPd8888O8::;. 'oOoo:.;..         ;:O88Ooo:' O\"'\n" +
+                "  ,8:   o::oO` 88888OOo:::  o8O8Oo:::;;     ,;:oO88OOo;  '\n" +
+                " ,YP  ,::;:O:  888888o::::  :8888Ooo::::::::::oo888888o;. ,\n" +
+                " ',d: :;;O;:   :888888::o;  :8888888Ooooooooooo88888888Oo; ,\n" +
+                " dPY:  :o8O     YO8888O:O:;  O8888888888OOOO888\"\" Y8o:O88o; ,\n" +
+                ",' O:  'ob`      \"8888888Oo;;o8888888888888'\"'     `8OO:.`OOb .\n" +
+                "'  Y:  ,:o:       `8O88888OOoo\"\"\"\"\"\"\"\"\"\"\"'           `OOob`Y8b`\n" +
+                "   ::  ';o:        `8O88o:oOoP                        `8Oo `YO.\n" +
+                "   `:   Oo:         `888O::oP                          88O  :OY\n" +
+                "    :o; 8oP         :888o::P                           do:  8O:\n" +
+                "   ,ooO:8O'       ,d8888o:O'                          dOo   ;:.\n" +
+                "   ;O8odo'        88888O:o'                          do::  oo.:\n" +
+                "  d\"`)8O'         \"YO88Oo'                          \"8O:   o8b'\n" +
+                " ''-'`\"            d:O8oK  -hrr-                   dOOo'  :o\":\n" +
+                "                   O:8o:b.                        :88o:   `8:,\n" +
+                "                   `8O:;7b,.                       `\"8'     Y:\n" +
+                "                    `YO;`8b'\n" +
+                "                     `Oo; 8:.\n" +
+                "                      `OP\"8.`\n" +
+                "                       :  Y8P\n" +
+                "                       `o  `,\n" +
+                "                        Y8bod.\n" +
+                "                        `\"\"\"\"'\n" +
+
+                "--------------------------------------------------------------------------\n" );
+
+
+
+    }
+}
